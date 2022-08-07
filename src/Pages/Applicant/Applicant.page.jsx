754e470899c8e8a14dcac489725c061fa35739a0
Carl Keneth Baseleres <baseleres.ck@gmail.com>
2022-08-08 07:33:39 +0800

    applicant page

--- src/Pages/Applicant/Applicant.page.jsx
+++ src/Pages/Applicant/Applicant.page.jsx
@@ -1,10 +1,12 @@
 import React, {useState, useEffect, useCallback} from 'react';
 import { useMsal } from '@azure/msal-react';
 import { useLocation, useResolvedPath } from 'react-router';
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button, Form, FormLabel, FormControl } from 'react-bootstrap'
 
 import { graphConfig, loginRequest } from '../../authConfig'
 import { callMsGraph } from '../../graph'
+
+import './Applicant.styles.css'
 
 const Applicant = () => {
     const location = useLocation().pathname.split('/')[useLocation().pathname.split('/').length - 1]
@@ -13,7 +15,17 @@ const Applicant = () => {
     const [ userData, setUserData ] = useState(null)
     const [ attachmentData, setAttachmentData ] = useState(null)
     const [ userInstance, setUserInstance ] = useState(null)
-    
+    const serviceLines = [
+        'Digital & Innovation',
+        'Application Development & Support',
+        'Finance Shared Services',
+        'Master Data Management'
+    ]
+
+    const availablePositions = [
+        'Analyst Programmer',
+        'Sr. Analyst Programmer'
+    ]
 
     // const fetchUser = useCallback(async () => {
     //     const user = await instance.acquireTokenSilent({
@@ -28,8 +40,13 @@ const Applicant = () => {
             
             // fetchUser()
             //     .catch(err => console.log('FETCH USER INSTANCE ERROR: ', err))
+            const user = await instance.acquireTokenSilent({
+                ...loginRequest,
+                account: accounts[0]
+            })
 
             const userData = await callMsGraph(user.accessToken, graphConfig.graphMessagesEndpoint + `/${location}`)
+            console.log("🚀 ~ file: Applicant.page.jsx ~ line 33 ~ fetchUserData ~ userData", userData)
             setUserData(userData)
         }
 
@@ -47,22 +64,25 @@ const Applicant = () => {
             //     .catch(err => console.log('FETCH USER INSTANCE ERROR: ', err))
 
             const attachmentData = await callMsGraph(user.accessToken, graphConfig.graphMessagesEndpoint + `/${location}/attachments`)
+            console.log("🚀 ~ file: Applicant.page.jsx ~ line 50 ~ fetchAttachmentData ~ attachmentData", attachmentData)
             setAttachmentData(attachmentData.value[0].contentBytes || "")
         }
 
         fetchAttachmentData()
             .catch(error => console.log('FETCH ATTACHMENT DATA ERROR: ', error))
-    },[userData, fetchUser])
+    },[userData])
     
     return(
         <Container>
-            <Row>
-                <Col></Col>
-                <Col>World</Col>
+            {/* <Row>
+                <Col>
+                    <h1>{userData && userData.sender.emailAddress.name}</h1> 
+                </Col>
+                <Col>{userData && userData.sender.emailAddress.name}</Col>
             </Row>
             <Row>
-                <Col>Email</Col>
-                <Col>World</Col>
+                <Col>{userData && userData.sender.emailAddress.address}</Col>
+                <Col>{userData && userData.sender.emailAddress.address}</Col>
             </Row>
             <Row>
                 <Col>Attachment</Col>
@@ -70,8 +90,50 @@ const Applicant = () => {
             </Row>
             <Row>
                 <Col>Set Meeting</Col>
-                <Col>World</Col>
-            </Row>
+                <Col>
+                    <Button>
+                        Set Meeting
+                    </Button>
+                </Col>
+            </Row> */}
+            <Form>
+                <Form.Group className='mb-3' controlId='formName'>
+                    <Form.Label>Applicant Name:</Form.Label>
+                    <Form.Control defaultValue={userData && userData.sender.emailAddress.name} disabled></Form.Control>
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='formEmail'>
+                    <Form.Label>Applicant Email Address:</Form.Label>
+                    <Form.Control defaultValue={userData && userData.sender.emailAddress.address} type='email' disabled></Form.Control>
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='content'>
+                    <Form.Label>Application Content Preview</Form.Label>
+                    <FormControl as="textarea" rows={3} defaultValue={userData && userData.bodyPreview} disabled></FormControl>
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='serviceLine'>
+                    <Form.Label>Service Line:</Form.Label>
+                    <Form.Control as="select" aria-label="Default select example">
+                        {serviceLines.map((data,i) => {
+                            return(<option key={i} value={data}>{data}</option>) 
+                        })}
+                        {/* <option>Digital and Innovation</option> */}
+                    </Form.Control>
+                    {/* <Form.Select>
+                        <option>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </Form.Select> */}
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='position'>
+                    <Form.Label>Position: </Form.Label>
+                    <Form.Control as="select" aria-label="Default select example">
+                        {availablePositions.map((data,i) => {
+                            return(<option key={i} value={data}>{data}</option>) 
+                        })}
+                        {/* <option>Digital and Innovation</option> */}
+                    </Form.Control>
+                </Form.Group>
+            </Form>
         </Container>
     )
 }
