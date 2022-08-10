e1db90d3ccd3e83147432887ed3aa8649a71c721
Carl Keneth Baseleres <baseleres.ck@gmail.com>
2022-08-11 07:20:14 +0800

    pdf link

--- src/Pages/Applicant/Applicant.page.jsx
+++ src/Pages/Applicant/Applicant.page.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useCallback} from 'react';
 import { useMsal } from '@azure/msal-react';
 import { useLocation, useResolvedPath } from 'react-router';
 import { Container, Row, Col, Button, Form, FormLabel, FormControl } from 'react-bootstrap'
+import DatePicker from 'react-date-picker'
 
 import { graphConfig, loginRequest } from '../../authConfig'
 import { callMsGraph } from '../../graph'
@@ -15,6 +16,12 @@ const Applicant = () => {
     const [ userData, setUserData ] = useState(null)
     const [ attachmentData, setAttachmentData ] = useState(null)
     const [ userInstance, setUserInstance ] = useState(null)
+    const [ availableMeetingTime, setAvailableMeetingTime] = useState(null)
+    const [ value, onChange ] = useState(new Date())
+    const [ availableTimes, setAvailableTimes ] = useState(null)
+    const [ availableEndTimes, setAvailableEndTimes ] = useState(null)
+    const [ formValue, setFormValue ] = useState(null)
+
     const serviceLines = [
         'Digital & Innovation',
         'Application Development & Support',
@@ -26,6 +33,17 @@ const Applicant = () => {
         'Analyst Programmer',
         'Sr. Analyst Programmer'
     ]
+    // let availableTimes = []
+    // const availableTimes = () => {
+    //     const times = [];
+    //     for (i=8;i=17;i++) {
+    //         times.push(`i:00:00`)
+    //     }
+    //     return times;
+    // }
+
+    
+
 
     // const fetchUser = useCallback(async () => {
     //     const user = await instance.acquireTokenSilent({
@@ -44,11 +62,22 @@ const Applicant = () => {
                 ...loginRequest,
                 account: accounts[0]
             })
-
+            
             const userData = await callMsGraph(user.accessToken, graphConfig.graphMessagesEndpoint + `/${location}`)
             console.log("🚀 ~ file: Applicant.page.jsx ~ line 33 ~ fetchUserData ~ userData", userData)
             setUserData(userData)
         }
+        let times = []
+        let endTimes = []
+        for (let i=8;i<17;i++) {
+            times.push(new Date(new Date(new Date(new Date().setHours(i)).setMinutes(0)).setSeconds(0)).toLocaleTimeString())
+        }
+        for (let i=9;i<18;i++) {
+            endTimes.push(new Date(new Date(new Date(new Date().setHours(i)).setMinutes(0)).setSeconds(0)).toLocaleTimeString())
+        }
+        
+        setAvailableTimes(times)
+        setAvailableEndTimes(endTimes)
 
         fetchUserData()
             .catch(error => console.log('FETCH USER DATA ERROR: ', error))
@@ -71,31 +100,21 @@ const Applicant = () => {
         fetchAttachmentData()
             .catch(error => console.log('FETCH ATTACHMENT DATA ERROR: ', error))
     },[userData])
+
+    useEffect(() => {
+        setFormValue({
+            name: userData && userData.sender.emailAddress.name,
+            email: userData && userData.sender.emailAddress.address,
+            bodyPreview: userData && userData.bodyPreview
+        })
+    },[attachmentData])
     
+    const handleChange = (e) => {
+        setFormValue(prev => ({...prev, [e.target.name]: e.target.value }))
+    }
+
     return(
         <Container>
-            {/* <Row>
-                <Col>
-                    <h1>{userData && userData.sender.emailAddress.name}</h1> 
-                </Col>
-                <Col>{userData && userData.sender.emailAddress.name}</Col>
-            </Row>
-            <Row>
-                <Col>{userData && userData.sender.emailAddress.address}</Col>
-                <Col>{userData && userData.sender.emailAddress.address}</Col>
-            </Row>
-            <Row>
-                <Col>Attachment</Col>
-                <Col>World</Col>
-            </Row>
-            <Row>
-                <Col>Set Meeting</Col>
-                <Col>
-                    <Button>
-                        Set Meeting
-                    </Button>
-                </Col>
-            </Row> */}
             <Form>
                 <Form.Group className='mb-3' controlId='formName'>
                     <Form.Label>Applicant Name:</Form.Label>
@@ -111,28 +130,41 @@ const Applicant = () => {
                 </Form.Group>
                 <Form.Group className='mb-3' controlId='serviceLine'>
                     <Form.Label>Service Line:</Form.Label>
-                    <Form.Control as="select" aria-label="Default select example">
+                    <Form.Control as="select" aria-label="Default select example" name='serviceLine' onChange={handleChange}>
                         {serviceLines.map((data,i) => {
                             return(<option key={i} value={data}>{data}</option>) 
                         })}
-                        {/* <option>Digital and Innovation</option> */}
                     </Form.Control>
-                    {/* <Form.Select>
-                        <option>Open this select menu</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </Form.Select> */}
                 </Form.Group>
                 <Form.Group className='mb-3' controlId='position'>
                     <Form.Label>Position: </Form.Label>
-                    <Form.Control as="select" aria-label="Default select example">
+                    <Form.Control as="select" aria-label="Default select example" name='position' onChange={handleChange}>
                         {availablePositions.map((data,i) => {
                             return(<option key={i} value={data}>{data}</option>) 
                         })}
-                        {/* <option>Digital and Innovation</option> */}
                     </Form.Control>
                 </Form.Group>
+                <Button>
+                    <a href={`data:application/pdf;base64,${attachmentData}`} target='_blank' >See Attachment</a>
+                </Button>
+                <h3>Set Meeting</h3>
+                <Form.Group className='mb-3' controlId='setTime'>
+                    <Form.Label>Date: </Form.Label>
+                    <DatePicker onChange={handleChange} name='meetingDate' value={value} />
+                    <Form.Control as="select" name='meetingStartTime' onChange={handleChange}>
+                        <option value={null}>Set Start Time:</option>
+                        {availableTimes && availableTimes.map((data,i) => {
+                            return(<option key={i} value={data}>{data}</option>)
+                        })}
+                    </Form.Control>
+                    <Form.Control as="select" name='meetingEndTime' onChange={handleChange}>
+                        <option values={null}>Set End Time:</option>
+                        {availableEndTimes && availableTimes.map((data,i) => {
+                            return(<option key={i} value={data}>{data}</option>)
+                        })}
+                    </Form.Control>
+                </Form.Group>
+                <Button type="submit">Save</Button>
             </Form>
         </Container>
     )
